e8d0caa32ec12081e66404e30954a1ec7bbf3c53
adrien <adrien.millot@yahoo.fr>
2025-03-17 21:34:46 +0100

    fix : refactor code to avoid code repetitions

--- src/main.cpp
+++ src/main.cpp
@@ -56,16 +56,29 @@ void initPreferences(Preferences& preferences) {
   if (!preferenceIntKeyExist(preferences, key_udp_port)) {
     udpPort = default_udp_port;
     preferences.putInt(key_udp_port, udpPort);
+  } else {
+    LOG_INFO("udpPort found and is not default values");
+    // Value converted from int_32 (preference answer format) to uint16 (udp port format):
+    udpPort = convertInt32ToUInt16(preferences.getInt(key_udp_port));
+    LOG_INFO("udpPort value after convertion: %d", udpPort);
   }
 
   if (!preferenceUint64_KeyExist(preferences, key_udp_msg_frequency)) {
+    LOG_INFO("udpMsgFreq does not exist or his default is 0 - use default time sleep");
     time_to_sleep = default_time_sleep;
     preferences.putULong64(key_udp_msg_frequency, time_to_sleep);
+  } else {
+    LOG_INFO("udpMsgFreq found and is not default values");
+    time_to_sleep = preferences.getULong64(key_udp_msg_frequency, default_time_sleep);
   }
 
   if (!preferenceStringKeyExist(preferences, key_udp_target_ip)) {
+    LOG_INFO("devideId does not exist or his default value is 192.168.1.255");
     udp_target_ip = default_udp_target_ip;
     preferences.putString(key_udp_target_ip, udp_target_ip);
+  } else {
+    LOG_INFO("devideId found and is not default values");
+    udp_target_ip = preferences.getString(key_udp_target_ip);
   }
 }
 
@@ -97,23 +110,6 @@ void setup() {
    */
   if (wifi_state == 200) {  // Connection wifi is working well -> Start DeepSleep cycle
     LOG_TRACE("WifiState equal to 200");
-
-    if (preferenceIntKeyExist(preferences, key_udp_port)) {
-      LOG_INFO("udpPort found and is not default values");
-      // Value converted from int_32 (preference answer format) to uint16 (udp port format):
-      udpPort = convertInt32ToUInt16(preferences.getInt(key_udp_port));
-      LOG_INFO("udpPort value after convertion: %d", udpPort);
-    } else {
-      udpPort = default_udp_port;
-    }
-
-    if (preferenceUint64_KeyExist(preferences, key_udp_msg_frequency)) {
-      LOG_INFO("udpMsgFreq found and is not default values");
-      time_to_sleep = preferences.getULong64(key_udp_msg_frequency, default_time_sleep);
-    } else {
-      LOG_INFO("udpMsgFreq does not exist or his default is 0 - use default time sleep");
-      time_to_sleep = default_time_sleep;
-    }
 
     initAHT10();  // Init T° and H sensors to do measure.
     if (wasWakeUpByButton()) {
